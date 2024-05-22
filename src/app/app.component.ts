a35f624db12e5a0be324fe2eced8388f26ea4e3b
ewertonluizmenesesrocha <ewertonluiz2009@hotmail.com>
2024-05-21 23:08:55 -0300

    feat: configurado PO UI no projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+} from '@po-ui/ng-components';
+import { ProtheusLibCoreModule } from '@totvs/protheus-lib-core';
+import { ProAppConfigService } from '@totvs/protheus-lib-core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule,
+    HttpClientModule,
+    ProtheusLibCoreModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  constructor(private proAppConfigService: ProAppConfigService) {
+    if (!this.proAppConfigService.insideProtheus()) {
+      this.proAppConfigService.loadAppConfig();
+    }
+  }
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Exit', action: this.closeApp.bind(this) },
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item');
+  }
+  private closeApp() {
+    if (this.proAppConfigService.insideProtheus()) {
+      this.proAppConfigService.callAppClose();
+    } else {
+      alert('O App não está sendo executado dentro do Protheus.');
+    }
+  }
+}
