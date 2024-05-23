cb4556284d99790f8610f54fe5cf85a907a56643
ewertonluizmenesesrocha <ewerton.rocha@asabranca.ind.br>
2024-05-23 12:06:05 -0300

    feat: button PoPageAction

--- src/app/shared/component/list-page/list-page.component.ts
+++ src/app/shared/component/list-page/list-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PoPageModule } from '@po-ui/ng-components';
+import { PoPageAction, PoPageModule } from '@po-ui/ng-components';
 import { PoTableModule } from '@po-ui/ng-components';
 
 @Component({
@@ -10,9 +10,18 @@ import { PoTableModule } from '@po-ui/ng-components';
   styleUrl: './list-page.component.scss'
 })
 export class ListPageComponent {
+  public readonly actions: Array<PoPageAction> = [
+    { label: 'Novo', action: this.cadObj.bind(this), disabled: true },
+    { label: 'Page', url: 'https://po-ui.io/documentation/po-page-list' }
+  ];
+
   readonly items = [
     { id: 1, name: 'Item 1', description: 'Descrição do Item 1' },
     { id: 2, name: 'Item 2', description: 'Descrição do Item 2' },
     { id: 3, name: 'Item 3', description: 'Descrição do Item 3' }
   ];
+
+  cadObj() {
+
+  }
 }
